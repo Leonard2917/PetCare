493cf18f41adcff460778d12627a14731ffb07fc
Loe <leonardmircea29@gmail.com>
2025-12-18 12:57:49 +0200

    startup

---
+++ PetCare/PetCare/Views/LoginView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+// ATENȚIE AICI: Trebuie să fie NumeProiect.Views
+namespace PetCareApp.Views
+{
+    public partial class LoginView : Page
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
